5111332ce4c156d1f55563d9b872824c6d5bf33e
Rodrigo Sartori <93sartori@gmail.com>
2023-09-17 16:49:57 -0300

    Implementação do InsertCustomer

---
+++ src/main/java/br/com/sartori/customers/dataprovider/mongodb/mapper/CustomerEntityMapper.java
@@ -0,0 +1,11 @@
+package br.com.sartori.customers.dataprovider.mongodb.mapper;
+
+import br.com.sartori.customers.core.domain.Customer;
+import br.com.sartori.customers.dataprovider.mongodb.entity.CustomerEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CustomerEntityMapper {
+
+    CustomerEntity toCustomerEntity(Customer customer);
+}
