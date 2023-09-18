d568a64ef8c80335dd766a4e20e8312a4d1eac35
Rodrigo Sartori <93sartori@gmail.com>
2023-09-17 22:04:02 -0300

    Implementação do dataprovider de Delete criado

---
+++ src/main/java/br/com/sartori/customers/dataprovider/impl/DeleteCustomerByIdImpl.java
@@ -0,0 +1,17 @@
+package br.com.sartori.customers.dataprovider.impl;
+
+import br.com.sartori.customers.core.dataprovider.DeleteCustomerById;
+import br.com.sartori.customers.dataprovider.mongodb.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class DeleteCustomerByIdImpl implements DeleteCustomerById {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+
+    @Override
+    public void delete(String id) {
+        customerRepository.deleteById(id);
+    }
+}
